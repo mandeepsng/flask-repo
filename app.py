c058b1d6de2f7295e52e174358a827a44784f9a1
mandeepsng <80085652+mandeepsng@users.noreply.github.com>
2023-07-12 12:15:42 +0000

    new update

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+import pyautogui
+import random
+import time
+
 
 app = Flask(__name__)
 
@@ -9,4 +13,17 @@ def hello_world():
 
 @app.route('/demo')
 def demo():
-    return 'testing'
+    random_time = random.randint(1, 5)
+
+    # create a time delay using the sleep()
+    # method
+    time.sleep(random_time)
+
+    # Take the screenshot using screenshot()
+    # method
+    myScreenshot = pyautogui.screenshot()
+
+    # Save the screenshot shot using current
+    # time as file name.
+    file_name = str(time.time())+".png"
+    myScreenshot.save(file_name)
